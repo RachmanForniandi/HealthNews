46feff8ac2dbfbaf18e1362b458eadbf46013c8e
RachmanForniandi <rachmanforniandi@gmail.com>
2021-03-31 01:58:37 +0700

    login,register,read,delete,click to detail berhasil.Insert-update masih gagal

--- app/src/main/java/rachmanforniandi/healthnews/ui/DetailActivity.kt
+++ app/src/main/java/rachmanforniandi/healthnews/ui/DetailActivity.kt
@@ -2,12 +2,18 @@ package rachmanforniandi.healthnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import com.bumptech.glide.Glide
 import com.rachmanforniandi.mahasiswacrudapp.model.read.ItemDataRead
+import kotlinx.android.synthetic.main.activity_add_update_data.*
+import kotlinx.android.synthetic.main.activity_detail.*
+import org.jetbrains.anko.image
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.support.v4.startActivity
+import rachmanforniandi.healthnews.BuildConfig
 import rachmanforniandi.healthnews.R
 
 class DetailActivity : AppCompatActivity() {
@@ -16,9 +22,16 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         val dataDetail = intent.getSerializableExtra("detailNews")as ItemDataRead
+        etDetailTitle.text = dataDetail.title
+        etDetailContentNews.text = dataDetail.content_news
+        etDetailAuthor.text = dataDetail.author
 
-
-
+        Glide.with(this)
+            .load(BuildConfig.URL_IMAGE_DATA + dataDetail.image)
+            .placeholder(R.drawable.placeholder_2)
+            .centerCrop()
+            .into(iv_detail_image)
+        Log.e("debugDetail",""+ BuildConfig.URL_IMAGE_DATA + dataDetail.image)
 
     }
 
